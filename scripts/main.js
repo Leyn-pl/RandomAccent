c6eaba7d3feb710ef9cba2565179f923d3fcb2a1
Leyn <65331501+Leyn-pl@users.noreply.github.com>
2025-04-09 15:38:47 +0300

    2.0

--- scripts/main.js
+++ scripts/main.js
@@ -10,16 +10,37 @@ const blacklist = [
 	"darkMetal",
 	"darkerMetal",
 	"darkestMetal"
-]
+];
+// Main function
 function RandomColor(color){
-	let pool = Object.keys(Pal);
-	blacklist.forEach(e => {
-		if(pool.includes(e)){
-			pool.pop(e)
-		};
+	let pool = []
+	Object.keys(Pal).forEach(key => {
+		if (!blacklist.includes(key)) pool.push(key);
 	});
 	let id = Math.floor(Math.random()*(pool.length+1));
 	Pal[color] = Pal[pool[id]];
+	// 2.0 changes
+	Colors.put(color, Pal[pool[id]]);
+	BuildDialog(NewColorDialog.cont, pool[id], String(Pal[pool[id]]))
 };
-// You can change any other color if you want to
-RandomColor("accent");
+var NewColorDialog;
+function BuildDialog(cont, clrName, clr){
+	cont.clear();
+	cont.image(Icon.pick, Pal[clrName]);
+	cont.add("[accent]Pal."+clrName).row();
+	cont.add("");
+	cont.add("#"+clr);
+}
+// Menu button to change color
+Events.on(ClientLoadEvent, () => {
+	// Create color showcase dialog
+	NewColorDialog = new BaseDialog("Pal.accent changed");
+	NewColorDialog.addCloseButton();
+	
+	Vars.ui.menufrag.addButton("Random Accent", Icon.pick, () => {;
+		RandomColor("accent");
+		NewColorDialog.show();
+	});
+	// You can change any other color if you want to
+	RandomColor("accent");
+});
